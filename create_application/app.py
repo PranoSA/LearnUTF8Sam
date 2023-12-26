81b9f741f68456af353459967d9be58503a63b3b
PranoSA <pcadler@gmail.com>
2023-12-25 20:14:49 -0800

    I do not know

--- create_application/app.py
+++ create_application/app.py
@@ -6,9 +6,12 @@ import os
 def lambda_handler(event, context):
 
     if 'AWS_SAM_LOCAL' in os.environ:
-        dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:4566')
+        #dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:4566', region_name='us-east-1')
+        dynamodb = boto3.resource('dynamodb', endpoint_url='http://172.17.0.1:4566', region_name='us-east-1')
+        #dynamodb = boto3.resource('dynamodb', endpoint_url='http://sd1230912391294124991.sdsad.123121:4566', region_name='us-east-1')
+        print('local \n')
     else:
-        dynamodb = boto3.resource('dynamodb')
+        dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
 
     table = dynamodb.Table('Applications')
 
@@ -25,25 +28,46 @@ def lambda_handler(event, context):
     description = body['description']
     conversions = body['conversions']
 
+    print(table)
+
+  #  return {
+        #'statusCode': 300,
+        #'body': json.dumps("Lets See if return")
+   # }
+
+    print(conversions)
+
     if not isinstance(conversions, list) or not all(isinstance(item, dict) and 'value' in item and isinstance(item['value'], str) for item in conversions):
         return {
             'statusCode': 400,
             'body': json.dumps('Invalid format for conversions. It should be a list of enumeratedConversations.')
         }
-
-    table.put_item(
-        Item={
-            'user_id': user_id,
-            'appid': appid,
-            'name': name,
-            'created_at': created_at,
-            'updated_at': updated_at,
-            'description': description,
-            'conversions': conversions
-        }
-    )
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Application created successfully!')
+    
+    Item={
+        'user_id': user_id,
+        'appid': appid,
+        'name': name,
+        'created_at': created_at,
+        'updated_at': updated_at,
+        'description': description,
+        'conversions': conversions
     }
+
+    try:
+        print("trying dynamo")
+        print(Item)
+
+        table.put_item(
+            Item=Item,
+        )
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Application created successfully!')
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Failed to Connect to DynamoDB')
+        }
